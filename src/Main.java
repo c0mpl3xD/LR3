405711b1df92bf751b9ac6549b0296a79c95201c
c0mpl3x <marszemlya2312@gmail.com>
2023-09-20 15:11:15 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //Task 1
-        System.out.println(Character.isLetterOrDigit('1'));
+        //System.out.println(Character.isLetterOrDigit('1'));
+
+        //Task 2
+        String emoji1 = "\uD83D\uDE10"; // U+1F610
+        String emoji2 = "\uD83D\uDE11"; // U+1F611
+        String emoji3 = "\uD83D\uDE15"; // U+1F615
+
+        System.out.println("Емоджі U+1F610: " + emoji1);
+        System.out.println("Емоджі U+1F611: " + emoji2);
+        System.out.println("Емоджі U+1F615: " + emoji3);
+
+        //Task 3
+
     }
 }
